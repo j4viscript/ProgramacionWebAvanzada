70a1e5ea53bb605d92499298160c88e85f60e07f
Javier Murillo <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-08-25 18:29:07 -0600

    Trabajo25agosto

---
+++ ProgramacionWebClases/trabajo25agosto/javascript/index.js
@@ -0,0 +1,215 @@
+var c=null;
+var ctx=null;
+var fig='arc';
+var press=false;
+var super_x=240;
+var super_y=240;
+var obstaculo1_x=70;
+var obstaculo1_y=160;
+var obstaculo2_x=90;
+var obstaculo2_y=60;
+var obstaculo3_x=440;
+var obstaculo3_y=400;
+var player1=null;
+var player2=null;
+var direction = 'right';
+var score = 0;
+var pause = false; 
+var perder = false;
+var obstaculo1 = null;
+var obstaculo2 = null;
+var obstaculo3 = null;
+var obstaculos = [];
+var speed = 2; 
+var bee = new Image();
+var flor = new Image();
+var wall = new Image();
+var play = new Audio();
+var gameover = new Audio();
+
+function paint(){
+    window.requestAnimationFrame(paint)
+    
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0,0,500,500);
+    ctx.font = "40px Modeka";
+    ctx.strokeText('Score: ' + score, 100,50);    
+    
+    //Players
+    // player1.dibujar(ctx);
+    // player2.dibujar(ctx);
+
+    //images
+    ctx.drawImage(bee, player1.x, player1.y, 50,50);
+    ctx.drawImage(flor, player2.x, player2.y, 50,50);
+    ctx.drawImage(wall, obstaculo1_x, obstaculo1_y, 50,200);
+    ctx.drawImage(wall, obstaculo2_x, obstaculo2_y, 150,30);
+    ctx.drawImage(wall, obstaculo3_x, obstaculo3_y,10,100);
+    //Obstaculos
+    // obstaculo1.dibujar(ctx);
+    // obstaculo2.dibujar(ctx);
+    // obstaculo3.dibujar(ctx);
+    if(pause){
+        ctx.beginPath();
+        ctx.fillStyle= "rgba(255,255,255,0.5)";
+        ctx.fillRect(0,0,500,500);
+        
+        ctx.beginPath();
+        ctx.fillStyle ='white';
+        ctx.fillText("P A U S E", 180,230);
+        speed = 0;
+    }else{    
+        update();
+    }
+
+    if(perder){
+        ctx.beginPath();
+        ctx.fillStyle= "rgba(255,255,255,0.5)";
+        ctx.fillRect(0,0,500,500);
+        
+        ctx.beginPath();
+        ctx.fillStyle ='black';
+        ctx.fillText("Perdiste", 180,230);
+    }
+    else{    
+        update();
+    }
+}
+function update(){
+    if(direction=='right'){
+        player1.x += speed;
+        if (player1.x>500){
+            player1.x=0;
+        }
+    }
+    if(direction == 'down'){
+        player1.y += speed;
+        if (player1.y>500){
+            player1.y=0;
+        }
+    }
+
+    if(direction=='left'){
+        player1.x -= speed;
+        if (player1.x<0){
+            player1.x=500;
+        }
+    }
+    if(direction=='up'){
+        player1.y -= speed;
+        if (player1.y<0){
+            player1.y=500;
+        }
+    }
+    if(player1.se_tocan(player2)){
+        player2.x = generateRandomInteger(500);
+        player2.y = generateRandomInteger(500);
+        play.play();
+        score +=10;
+    }
+    for(i = 0; i < obstaculos.length; i++){
+        if(player1.se_tocan(obstaculos[i])){
+            perder = true;
+            speed = 0;
+            console.log("Si jala");
+            gameover.play();
+        }
+    }
+    
+}
+document.addEventListener('keydown', function (e) {
+    console.log(e);
+// Arriba
+    if(e.keyCode==87 || e.keyCode==38){
+        direction = 'up';
+    }
+// Abajo
+    if(e.keyCode==83 || e.keyCode==40){
+        direction = 'down';
+    }
+    // Izquierda
+    if(e.keyCode==65 || e.keyCode==37){
+        direction = 'left';
+    }
+
+    // Derecha
+    if(e.keyCode==68 || e.keyCode==39){
+        direction = 'right';
+
+    }
+    //Pausa
+    if(e.keyCode==32){ 
+        pause = (pause)?false:true;
+        speed = 2;
+    }
+
+});
+
+window.addEventListener('load', start);
+
+function start(){
+    c=document.getElementById("myCanvas");
+    ctx=c.getContext("2d");
+    player1 = new Cuadrado(super_x, super_y, 40, 40, 'white');
+    player2 = new Cuadrado(generateRandomInteger(500),generateRandomInteger(500), 40,40,'blue');
+    
+    obstaculo1 = new Cuadrado(obstaculo1_x,obstaculo1_y, 50,200,'yellow');
+    obstaculo2 = new Cuadrado(obstaculo2_x, obstaculo2_y, 150,30,'yellow');
+    obstaculo3 = new Cuadrado(obstaculo3_x, obstaculo3_y, 10,100,'yellow');
+    obstaculos = [obstaculo1, obstaculo2, obstaculo3];
+
+    bee.src = '/trabajo25agosto/src/bee.png';
+    flor.src = '/trabajo25agosto/src/flor.png';
+    wall.src = '/trabajo25agosto/src/wall.png';
+    play.src = '/trabajo25agosto/src/audio.mp3';
+    gameover.src = '/trabajo25agosto/src/perder-incorrecto-no-valido.mp3';
+    paint();
+}
+
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 17);
+        };
+}());
+
+function Cuadrado(x,y,w,h,color){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color= color;
+    this.se_tocan = function (target) { 
+
+
+
+        if(this.x < target.x + target.w &&
+        
+           this.x + this.w > target.x && 
+        
+           this.y < target.y + target.h && 
+        
+           this.y + this.h > target.y)
+        
+        {
+        
+        return true;  
+        
+        }  
+        
+        };
+    
+    this.dibujar = function(ctx){
+        ctx.fillStyle = this.color;
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+
+}
+
+function generateRandomInteger(max){
+    return Math.floor(Math.random() * max);
+}
